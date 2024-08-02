9ad68860ad94924c950aecc5249937affc023d1f
정승민 <103707732+jjas-min@users.noreply.github.com>
2024-08-02 10:09:32 +0900

    Update hier2ply.py

--- hier2ply.py
+++ hier2ply.py
@@ -45,7 +45,7 @@ class GaussianModel:
     def save_ply(self, path):
         mkdir_p(os.path.dirname(path))
 
-        xyz = self._xyz.detach().cpu().numpy()
+        xyz = self._xyz.detach().cuda().numpy()
         normals = np.zeros_like(xyz)
         f_dc = self._features_dc.detach().transpose(1, 2).flatten(start_dim=1).contiguous().cpu().numpy()
         f_rest = self._features_rest.detach().transpose(1, 2).flatten(start_dim=1).contiguous().cpu().numpy()
